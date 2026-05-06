85d2924098528dc05c6d10b23ae26fe4f585a110
zhLyan <102575196+zhLyan@users.noreply.github.com>
2026-05-06 11:41:55 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < s.size(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
+    }
+};
